fcbb53f33a8a93395b029a17429a67e785f6d8df
Cedric Krause <cedware-dev@protonmail.com>
2025-07-19 20:55:10 +0200

    test

--- Formula/bountui.rb
+++ Formula/bountui.rb
@@ -1,8 +1,8 @@
 class Bountui < Formula
   desc "A boundary terminal ui"
   homepage "https://github.com/Cedware/bountui"
-  url "https://github.com/Cedware/bountui/releases/download/v0.4.0/bountui-0.4.0-aarch64-apple-darwin.zip"
-  sha256 "5214f262d19ff06d8dce3815a4a085a8ddf47a9c04ee10bfc4650e3df778f5a1"
+  url "test"
+  sha256 "test"
   license "GPL-3.0"
 
   depends_on "hashicorp/tap/boundary"
--- test.sh
+++
@@ -1,8 +0,0 @@
-bin_archive=https://github.com/Cedware/bountui/releases/download/v0.4.0/bountui-0.4.0-aarch64-apple-darwin.zip
-curl -L $bin_archive -o bin.zip
-checksum=$(sha256sum bin.zip | cut -d ' ' -f 1)
-echo "checksum: $checksum"
-sed -i "s|^  url \".*\"|  url \"${bin_archive}\"|" Formula/bountui.rb
-sed -i "s|^  sha256 \".*\"|  sha256 \"${checksum}\"|" Formula/bountui.rb
-git add .
-git commit -m "Updated bountui formula to version x.y.z"
